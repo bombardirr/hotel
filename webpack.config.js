e0bc142027c203454edfedd24e3ae1a2138e1104
AG <artem.golovin1@gmail.com>
2020-07-23 17:28:31 +0900

    Webpack settings done. Finally

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,71 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const LiveReloadPlugin = require('webpack-livereload-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
-// const PATHS = {
-//   src: path.resolve(__dirname, '../src'),
-//   dist: path.resolve(__dirname, '../dist')
-// }
+
+const isDev = process.env.NODE_ENV === 'development'
+const isProd = !isDev
+
+const optimization = () => {
+  const config = {
+    splitChunks: {
+      chunks: "all"
+    }
+  }
+  if (isProd) {
+    config.minimizer = [
+      new TerserPlugin(),
+      new OptimizeCssAssetsPlugin()
+    ]
+  }
+  return config
+}
+
+const filename = ext => isDev ? `[name].${ext}` : `[name].[hash].${ext}`
+
+const cssLoaders = extra => {
+  const loaders = [
+    {
+      loader: MiniCssExtractPlugin.loader,
+      options: {
+        hmr: isDev,
+        reloadAll: true
+      }
+    },
+    'css-loader'
+  ]
+  if (extra) {
+    loaders.push(extra)
+  }
+  return loaders
+}
 
 module.exports = {
-  entry: './src/index.js',
+  optimization: optimization(),
+  devtool: isDev ? 'source-map' : '',
+  context: path.resolve(__dirname, 'src'),
+  entry: './index.js',
   output: {
-    filename: '[name].bundle.js',
+    filename: filename('js'),
     path: path.resolve(__dirname, 'dist')
   },
+  devServer: {
+    port: 4200,  // can be any port
+    hot: isDev
+  },
   plugins: [
+    new LiveReloadPlugin({
+      appendScriptTag: true
+    }),
+    new MiniCssExtractPlugin({
+      filename: filename('css')
+    }),
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'src/pug/pages/index.pug')
     }),
@@ -21,8 +74,35 @@ module.exports = {
     rules: [
       {
         test: /\.pug$/,
-        use: ['pug-loader']
+        loader: 'pug-loader',
+        query: {
+          pretty: true
+        }
       },
+      {
+        test: /\.css$/,
+        use: cssLoaders()
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: cssLoaders('sass-loader')
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              '@babel/preset-env'
+            ]
+          }
+        }
+      },
+      {
+        test: /\.(png|jpg|svg|gif)/,
+        use: ['file-loader']
+      }
     ]
   }
 }
